9cbc7fe93b36b7a9cc55ca4a69418fd57aff4ca4
gihyo-san <gihyo-san@example.com>
2026-03-03 08:51:44 +0900

    検索追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import mdx from '@astrojs/mdx';
+import pagefind from "astro-pagefind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx()]
+  integrations: [mdx(), pagefind()]
 });
